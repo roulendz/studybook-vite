4c262c6ffbd6c872fb8ee924d6b4bb7d270f8d73
roulendz <rolands.zeltins@inbox.lv>
2023-04-25 23:33:24 +0300

    added src dir to assets

---
+++ assets/src/js/app.js
@@ -0,0 +1,6 @@
+import '../../../../../modules/system/assets/js/framework';
+import '../../../../../modules/system/assets/js/framework-extras';
+// import 'flowbite';
+// import '../../../assets/src/css/app.scss';
+// import '../../../partials/main-menu/main-menu';
+
